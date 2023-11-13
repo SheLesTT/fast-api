a654057c0b8d78907e244ce3b1588f3d26f7fab8
SheLesTT <lovsevdenis3513@gmail.com>
2023-11-13 15:33:39 +0300

    set up pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from app.main import app
+from ..app.main import app
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
